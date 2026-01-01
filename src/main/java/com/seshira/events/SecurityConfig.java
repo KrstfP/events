e57740425d48150ebb710660c82aa9655ac12fb1
krstf <krstfp@proton.me>
2026-01-01 11:36:05 +0100

    feat: fix qg

--- src/main/java/com/seshira/events/SecurityConfig.java
+++ src/main/java/com/seshira/events/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.seshira.events;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,9 +11,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${security.csrf.enabled:true}")
+    private boolean csrfEnabled = true;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        if (!csrfEnabled) http.csrf(csrf -> csrf.disable());
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
